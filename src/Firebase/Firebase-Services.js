e4233e33580a9250f4204e786ce8f450fbb70aaf
Abdul Hanan <hanannawaz0@gmail.com>
2022-07-05 14:33:31 +0500

    Add Topic Added and Worked Fine

--- src/Firebase/Firebase-Services.js
+++ src/Firebase/Firebase-Services.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyARApeIKSQvWBnajfhaHGcMxCrhp1d3pAk",
@@ -13,5 +14,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
+export const storage = getStorage(app);
 
-export const db = getFirestore(app);;
--- src/Firebase/Topics.js
+++ src/Firebase/Topics.js
@@ -1,11 +1,10 @@
 import { db } from './Firebase-Services'
-import { collection, addDoc, getDocs } from 'firebase/firestore'
+import { doc, getDocs, setDoc } from 'firebase/firestore'
 
-const collectionref =  collection(db, "Topics");
 
 class Topics {
-    addtopic(newTopic){
-        return addDoc(collectionref, newTopic);
+    addtopic(Name,newTopic){
+        return setDoc(doc(db, "Topics",Name), newTopic);
     }
 
     getTopics(){
