2896d3b2176cdc470aad7c0e9b7c34ab8f8caf06
Abdul Hanan <hanannawaz0@gmail.com>
2022-07-09 22:27:15 +0500

    Mcqs are shown on Test Page after Click take test on 'topics-list' page

--- src/Dashboard/TakeTest.js
+++ src/Dashboard/TakeTest.js
@@ -1,8 +1,93 @@
-import React from 'react'
+import { doc } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import { Col, Container, Label, Row } from 'reactstrap'
+import Mcqs from '../Firebase/Add-Mcqs';
+
+function useQuery() {
+    return new URLSearchParams(useLocation().search);
+}
 
 function TakeTest() {
+
+    let query = useQuery();
+    // const Email = query.get("Email");
+    // const ID = query.get("ID");
+    // const LVL = query.get("lvl");
+    const Topic = query.get("Topic");
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const getallMcqs = async () => {
+            const docSnap = await Mcqs.getmcqs(Topic);
+            setData(docSnap.docs.map((doc) => (
+                {
+                ...doc.data(),  id: doc.id
+            })))
+
+        }
+
+        getallMcqs();
+    })
+
   return (
-    <div>TakeTest</div>
+    <Container>
+        <Row>
+            <Col>
+                <h5 className=' text-center text-primary'> Test for { Topic } </h5>
+            </Col>
+        </Row>
+
+        {data.map((doc,index) => {
+            return(
+                <div className='container' key={doc.id}>
+                    
+                <Row>
+                    <Col>
+                        <Label className='text-primary h6' > Question:{index+1}</Label>
+                        <h4> {doc.mcq} </h4>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm={6}>
+                    <div class="form-check">
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"/>
+                    <label class="form-check-label" for="flexRadioDefault1">
+                        {doc.Option1}
+                    </label>
+                    </div>
+                    </Col>
+                    <Col sm={6}>
+                    <div class="form-check">
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2"/>
+                    <label class="form-check-label" for="flexRadioDefault2">
+                    {doc.Option2}
+                    </label>
+                    </div>
+                    </Col>
+                    <Col sm={6}>
+                    <div class="form-check">
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault3"/>
+                    <label class="form-check-label" for="flexRadioDefault2">
+                    {doc.Option3}
+                    </label>
+                    </div>
+                    </Col>
+                    <Col sm={6}>
+                    <div class="form-check">
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault4"/>
+                    <label class="form-check-label" for="flexRadioDefault2">
+                    {doc.Option4}
+                    </label>
+                    </div>
+                    </Col>
+                </Row>
+            </div>
+           
+        
+            )
+        })}
+    </Container>
   )
 }
 
--- src/Firebase/Add-Mcqs.js
+++ src/Firebase/Add-Mcqs.js
@@ -1,9 +1,13 @@
 import { db } from './Firebase-Services'
-import { doc, setDoc } from  'firebase/firestore'
+import { collection, doc, setDoc, getDocs } from  'firebase/firestore'
 
 class Mcqs  {
     addmcq (mcqtag, Topic, newMcq) {
         return setDoc(doc(db, `${Topic}` , mcqtag) , newMcq);
+    }
+    getmcqs (Topic) {
+        const collectionref = collection(db, `${Topic}`);
+        return getDocs(collectionref);
     }
 }
 
