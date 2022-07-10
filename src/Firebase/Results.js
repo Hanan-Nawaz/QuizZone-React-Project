230bfc5c76b3fd593a21fbd92fc2b450d00203ec
Abdul Hanan <hanannawaz0@gmail.com>
2022-07-10 12:22:04 +0500

    Get Results function Added

--- src/Firebase/Results.js
+++ src/Firebase/Results.js
@@ -1,9 +1,13 @@
-import { doc, setDoc } from "firebase/firestore"
+import { collection, doc, getDocs, setDoc } from "firebase/firestore"
 import { db } from "./Firebase-Services"
 
 class Results {
     addResult(Email, Topic, newResult ){
         return setDoc(doc(db, `${Email}`, `${Topic}`) , newResult);
+    }
+    getResults(Email){
+        const collref = collection(db, `${Email}`);
+        return getDocs(collref);
     }
 }
 
