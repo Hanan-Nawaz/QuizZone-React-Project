eb07eda573e00da00d144aa0d4790e5afafb2503
Abdul Hanan <hanannawaz0@gmail.com>
2022-07-12 16:12:02 +0500

    Testimonials Page Added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Slider from './Slider'
 import DownloadApp from './DownloadApp'
 import Price from './Price'
+import Testimonials from './Testimonials'
 
 function Home() {
   return (
     <>
     <Slider />
     <Price/>
+    <Testimonials/>
     <DownloadApp/>
     </>
   )
